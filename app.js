2da9f7fe9e5bc6f8a7123be0642d3f507ebb492f
Luis Angel <vagaluis34@gmail.com>
2024-01-25 11:38:10 -0600

    Completando funciones de Encriptar, Desencriptar y Copiar, dandole un rango al textarea con solo minusculas

--- app.js
+++ app.js
@@ -1,41 +1,72 @@
+let textoProcesado = document.getElementById('textoProcesado');
+
 function encriptar(){
     let fraseUsuario = document.getElementById('ingresoUsuario').value;
-    let fraseEncriptada;
+    let fraseEncriptada = '';
 
-    console.log(fraseUsuario);
-
-    for(i=0; i<fraseUsuario.lenght; i++){
+    for(i=0; i<fraseUsuario.length; i++){
         if(fraseUsuario.charAt(i) == 'a'){
-            fraseEncriptada = fraseEncriptada.concat("ai");
+            fraseEncriptada = fraseEncriptada + "ai";
         }
         else if(fraseUsuario.charAt(i) == 'e'){
-            fraseEncriptada = fraseEncriptada.concat("enter");
+            fraseEncriptada = fraseEncriptada + "enter";
         }
         else if(fraseUsuario.charAt(i) == 'i'){
-            fraseEncriptada = fraseEncriptada.concat("imes");
+            fraseEncriptada = fraseEncriptada + "imes";
         }
         else if(fraseUsuario.charAt(i) == 'o'){
-            fraseEncriptada = fraseEncriptada.concat('ober');
+            fraseEncriptada = fraseEncriptada + "ober";
         }
         else if(fraseUsuario.charAt(i) == 'u'){
-            fraseEncriptada = fraseEncriptada.concat('ufat')
+            fraseEncriptada = fraseEncriptada + "ufat";
         }
         else{
             fraseEncriptada = fraseEncriptada.concat(fraseUsuario.charAt(i));
         }
-    
-        let f = document.querySelector(textarea);
-        f.innerHTML = fraseEncriptada;
     }
-    
 
-    console.log(fraseEncriptada);
+    textoProcesado.innerHTML = fraseEncriptada;
+    document.getElementById('copiar').removeAttribute('disabled');
 }
 
 function desencriptar(){
+    let fraseUsuario = document.getElementById('ingresoUsuario').value;
+    let fraseEncriptada = '';
 
+    for(i=0; i<fraseUsuario.length; i++){
+        if(fraseUsuario.charAt(i) == 'a'){
+            fraseEncriptada = fraseEncriptada + fraseUsuario.charAt(i);
+            i++;
+        }
+        else if(fraseUsuario.charAt(i) == 'e'){
+            fraseEncriptada = fraseEncriptada + fraseUsuario.charAt(i);
+            i = i + 4;
+        }
+        else if(fraseUsuario.charAt(i) == 'i'){
+            fraseEncriptada = fraseEncriptada + fraseUsuario.charAt(i);
+            i = i + 3
+        }
+        else if(fraseUsuario.charAt(i) == 'o'){
+            fraseEncriptada = fraseEncriptada + fraseUsuario.charAt(i);
+            i = i + 3;
+        }
+        else if(fraseUsuario.charAt(i) == 'u'){
+            fraseEncriptada = fraseEncriptada + fraseUsuario.charAt(i);
+            i = i + 3
+        }
+        else{
+            fraseEncriptada = fraseEncriptada.concat(fraseUsuario.charAt(i));
+        }
+    }
+
+    textoProcesado.innerHTML = fraseEncriptada;
+    document.getElementById('copiar').removeAttribute('disabled');
 }
 
 function copiar(){
-
+    let copyText = document.querySelector("#textoProcesado");
+    copyText.select();
+    document.execCommand("copy");
 }
+      
+document.querySelector("#copy").addEventListener("click", copy);
